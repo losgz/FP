2dcf9c1c7d863cf6cfd7c037d64b8848cdeb1363
Losg <145228057+losgz@users.noreply.github.com>
2023-12-05 20:46:10 +0000

    Add files via upload

---
+++ Proj.py
@@ -0,0 +1,5 @@
+# P6G01
+
+import requests
+
+base_url = 'https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=ac1fbe548edb4eb2bf8f5642519ac399'
