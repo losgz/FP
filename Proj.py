bcd49f470dd1e10f0474dbc97e8e726d759a8e46
Oxx05 <bernardoam05@ua.pt>
2023-12-07 16:47:27 +0000

    a

--- Proj.py
+++ Proj.py
@@ -2,4 +2,5 @@
 
 import requests
 
-base_url = 'https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=ac1fbe548edb4eb2bf8f5642519ac399'
+base_url = requests.get('https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=ac1fbe548edb4eb2bf8f5642519ac399')
+print("resp.text:\n", base_url.text[:300])
