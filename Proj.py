5196e2798ed860f5b0ba793e5c6c40cfb4c3bc37
Oxx05 <bernardoam05@ua.pt>
2023-12-09 13:17:48 +0000

    a

--- Proj.py
+++ Proj.py
@@ -1,6 +1,34 @@
-
 import requests
-resp = requests.get("https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=ac1fbe548edb4eb2bf8f5642519ac399")
-a=(resp.json())['features'][0]['properties'].values()
-# print("resp.text:\n", (resp.json())['features'][0])
-print(a)
+def ficheiro():
+    lst=[]
+    with open ('categories.txt','r') as details:
+        for l in details:
+            line=l.split('\n')
+            line="".join(line)
+            lst.append(line)
+        
+
+        # det={i for i in lst}
+    return(lst)
+lat=48.56
+lon=26.31
+url="https://api.geoapify.com/v2/places"
+
+resp = requests.get("https://api.geoapify.com/v2/places")
+category=ficheiro()[1]
+dic=f'?categories={category}&filter=rect%3A{lat}%2C{lon}%2C{lat}%2C{lon}&limit=100&apiKey=ac1fbe548edb4eb2bf8f5642519ac399'
+print(f"{url+dic}")
+response = requests.get(f"{url+dic}")
+print(response.json())
+
+def ficheiro():
+    lst=[]
+    with open ('categories.txt','r') as details:
+        for l in details:
+            line=l.split('\n')
+            line="".join(line)
+            lst.append(line)
+        
+
+        det={i for i in lst}
+    print(det)
