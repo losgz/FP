b5c739eb60873e404508349cbdd455e2a9c5910c
Oxx05 <bernardoam05@ua.pt>
2023-12-09 14:25:11 +0000

    a

--- Proj.py
+++ Proj.py
@@ -10,13 +10,15 @@ def ficheiro():
 
         # det={i for i in lst}
     return(lst)
-lat=48.56
-lon=26.31
+lat=40.457407
+lon=-8.451328
+km=30
+radius=km*1000
 url="https://api.geoapify.com/v2/places"
 
 resp = requests.get("https://api.geoapify.com/v2/places")
-category=ficheiro()[1]
-dic=f'?categories={category}&filter=rect%3A{lat}%2C{lon}%2C{lat}%2C{lon}&limit=100&apiKey=ac1fbe548edb4eb2bf8f5642519ac399'
+category=ficheiro()[0]
+dic=f'?categories={category}&filter=circle:{lon},{lat},{radius}&limit=20&apiKey=ac1fbe548edb4eb2bf8f5642519ac399'
 print(f"{url+dic}")
 response = requests.get(f"{url+dic}")
 print(response.json())
