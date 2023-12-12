704c8273863339a0ff0a55655fd6cb67f1c23431
Losg <145228057+losgz@users.noreply.github.com>
2023-12-12 15:05:20 +0000

    Create Proj2.py

---
+++ Proj2.py
@@ -0,0 +1,103 @@
+# P6G01
+import requests
+import math
+
+
+URL = "https://api.geoapify.com/v2/places"
+
+
+def location(url, latitude, longitude, radius, categories):
+    dic = f"?categories={categories}&filter=circle:{longitude},{latitude},{radius}&limit=50&apiKey=ac1fbe548edb4eb2bf8f5642519ac399"
+    response = requests.get(f"{url+dic}")
+    return response.json()
+
+
+def distance(lat1, lon1, lat2, lon2):
+    distance = math.sqrt((lat2 - lat1) ** 2 + (lon2 - lon1) ** 2)
+    return distance
+
+
+def formated_location(URL, latitude, longitude, radius, categories):
+    lst = []
+    counter = 0
+    for i in categories:
+        all_info = location(URL, latitude, longitude, radius, i)
+    all_info = all_info.get("features")
+    for i in all_info:
+        info = i.get("properties")
+        lat1 = info.get("lat")
+        lon1 = info.get("lon")
+        dist = distance(lat1, lon1, latitude, longitude)
+        if not info.get("name"):
+            continue
+        tpl = (
+            (info.get("name")),
+            (info.get("country")),
+            (info.get("city")),
+            (f"{lat1:.2f}"),
+            (f"{lon1:.2f}"),
+            (dist),
+            (info.get("postcode")),
+            (info.get("street")),
+            (info.get("formatted")),
+        )
+        counter += 1
+        lst.append(tpl)
+    return counter, lst
+
+
+def sort_attractions(lst , x):
+    if x == 0:
+        lst.sort(key=lambda y: y[0])
+    elif x == 1:
+        lst.sort(key=lambda y: y[1])
+    elif x == 2:
+        lst.sort(key=lambda y: y[2])
+    elif x == 3:
+        lst.sort(key=lambda y: y[5])
+    for i in lst:
+        print(f"{i}")
+    return lst
+
+
+def avg_distance(lst):
+    total = 0
+    for i in lst:
+        total += i[5]
+    return total / len(lst)
+
+
+def main():
+    print("Welcome to our program!")
+    print("Based on your preference, we will show you the best attractions for you.")
+    print(
+        "Do you want to sort the attractions by: \n[0]-Name \n[1]-Country\n[2]-City\n[3]-Distance\n"
+    )
+    option = input("Choose an option: ")
+    if not option.isdigit() or int(option) > 3 or int(option) < 0:
+        print("\nInvalid input, please choose an option form 0 to 3.\n")
+        main()
+    else:
+        option =  int(option)
+
+    coordinates = input(
+        "Insert the latitude and longitude (split them with a comma): "
+    ).split(",")
+    radius = int(input("Insert the radius: "))
+    categories = input("Insert the categories (split them with a comma): ").split(",")
+    latitude = float(coordinates[0])
+    longitude = float(coordinates[1])
+    try:
+        x = formated_location(URL, latitude, longitude, radius, categories)
+        sort_attractions(x[1], option)
+        print(f"\nNumber of locations: {x[0]}")
+        print(f"Average distance: {avg_distance(x[1]):.4f}")
+    except:
+        print("\nInvalid categories, see the valid categories below: \n")
+        with open("P6G01/Projeto02/categories.txt", "r") as f:
+            for line in f:
+                print(line.strip())
+
+
+if __name__ == "__main__":
+    main()
